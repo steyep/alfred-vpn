4bd4ddf79ba1d630e86b280b530425de5c6de7e9
Stephen Pennell <stepenn7@gmail.com>
2016-08-01 15:58:34 -0400

    Adds check to ensure Cisco AnyConnect exists

--- _scripts/wf_menu.sh
+++ _scripts/wf_menu.sh
@@ -1,4 +1,17 @@
 q="$1"
+
+# Make sure the CLI exists
+test ! -f /opt/cisco/anyconnect/bin/vpn && cat << EOF
+<?xml version="1.0"?>
+  <items>
+  <item valid="no">
+      <title>This workflow requires Cisco AnyConnect VPN</title>
+      <subtitle></subtitle>
+      <icon>_icons/warning.png</icon>
+    </item>
+  </items>
+EOF
+
 read -a arr <<< "$(echo "${q// }" | sed 's/►/ /g' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
 len=$(( $(echo "${#arr[@]}") -1 ))
 
